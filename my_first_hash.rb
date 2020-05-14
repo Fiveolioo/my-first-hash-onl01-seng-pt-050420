04959f9d72770d7843c6a3d4532708ee12bf567e
Richard Cho <96richardohc@gmail.com>
2020-05-14 05:28:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"Dogs" => "Momi", "Chico", "Kehau", "People" => "Richard", "Jamie"}
+my_hash = {"Dogs"=>"Momi", "Chico", "Kehau", "people"=>"Richard", "Jamie"}
   
 end
 
